0e1b0c60c8a745e26d2c799af6d0d68c5f102956
yewon <yewon1914@gmail.com>
2023-03-27 21:25:15 +0900

    script defer & use strict

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+// Whole-script strict mode syntax
+// JavaScript is very flexible
+// flexible === dangerous
+// added ECMAScript 5
+'use strict';
+console.log('Hello world!');
+
+let a; //use strict를 사용 시, 변수 정의 필요'
+a = 6;
