356efb7ab047e0bb7ab462736bad0925fe51ebc4
yewon <yewon1914@gmail.com>
2023-05-01 16:35:06 +0900

    Inheritance study

--- javascript/class.js
+++ javascript/class.js
@@ -80,3 +80,48 @@ const article2 = new Article(2);
 console.log(article1.publisher); //undefined
 console.log(Article.publisher);
 Article.printPublisher();
+
+// 5. Inheritance
+// a way for one class to extend another class.
+class Shape {
+    constructor(width, heignt, color) {
+        this.width = width;
+        this.heignt = heignt;
+        this.color = color;
+    }
+
+    draw() {
+        console.log(`drwing ${this.color} color!`);
+    }
+
+    getArea() {
+        return this.width * this.heignt;
+    }
+}
+
+class Rectangle extends Shape {}
+class Triangle extends Shape {
+    // 오버라이딩(Overriding) : 상위 클래스가 가지고 있는 메서드를 하위 클래스가 재정의해서 사용
+    draw() {
+        // super : 상위 클래스가 가지고 있는 메소드 호출
+        super.draw()
+        console.log('▲');
+    }
+    getArea() {
+        return (this.width * this.heignt) / 2;
+    }
+}
+
+const rectangle = new Rectangle(20, 20, 'blue');
+rectangle.draw();
+console.log(rectangle.getArea());
+const triangle = new Triangle(20, 20, 'red');
+rectangle.draw();
+console.log(triangle.getArea());
+
+// 6. Class checking : instanceOf
+console.log(rectangle instanceof Rectangle); // T
+console.log(triangle instanceof Rectangle); // F
+console.log(triangle instanceof Triangle); // T
+console.log(triangle instanceof Shape); // T
+console.log(triangle instanceof Object) // T
