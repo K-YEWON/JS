09ddebaf7b8c23ad1063ec8f0753fd51c9fd60c9
yewon <yewon1914@gmail.com>
2023-06-10 13:01:26 +0900

    js apis

--- javascript/array-api.js
+++ javascript/array-api.js
@@ -54,31 +54,71 @@
   
   // Q5. find a student with the score 90
   {
+    const result =  students.find(function(student, index) {
+      return student.score === 90;
+    }); 
+    console.log(result);
   }
-  
+  // Arrow function 변환
+  {
+    const result = students.find((student) => student.score === 90);
+    console.log(result);
+  }
   // Q6. make an array of enrolled students
   {
+    const result = students.filter((student) => student.enrolled);
+    console.log(result);
   }
   
   // Q7. make an array containing only the students' scores
   // result should be: [45, 80, 90, 66, 88]
   {
+    const result = students.map((student) => student.score);
+    console.log(result);
   }
   
   // Q8. check if there is a student with the score lower than 50
   {
+    console.clear();
+    const result = students.some((student) => student.score < 50);
+    console.log(result);
+
+    const result2 = students.every((student) => student.score < 50);
+    console.log(result2);
   }
   
+  console.clear();
   // Q9. compute students' average score
   {
+    const result = students.reduce((prev, curr) => {
+      console.log('------');
+      console.log(prev);
+      console.log(curr);
+      return prev + curr.score;
+    }, 0);
+    console.log(result);
+  }
+
+  {
+    const result = students.reduce((prev, curr) => prev + curr.score, 0);
+    console.log(result / students.length);
   }
   
   // Q10. make a string containing all the scores
   // result should be: '45, 80, 90, 66, 88'
   {
+    const result = students
+    .map((student) => student.score)
+    .filter((score) => score >= 50)
+    .join();
+    console.log(result);
   }
   
   // Bonus! do Q10 sorted in ascending order
   // result should be: '45, 66, 80, 88, 90'
   {
+    const result = students.map(student => student.score)
+    .sort((a, b) => a - b)
+    .join();
+    console.log(result);
   }
